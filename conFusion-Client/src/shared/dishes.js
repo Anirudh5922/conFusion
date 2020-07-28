fff0049e36d7fecdb0311ae0e092492162b167e4
Anirudh Maheshwari <43237488+Anirudh5922@users.noreply.github.com>
2020-07-28 23:51:55 +0530

    Add files via upload

---
+++ conFusion-Client/src/shared/dishes.js
@@ -0,0 +1,84 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+        },
+        {
+        id: 1,
+        name:'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+        id: 2,
+        name:'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        },
+        {
+        id: 4,
+        name:'Samosa Chaat',
+        image: '/assets/images/samosachaat.jpg',
+        category: 'appetizer',
+        label:'',
+        price:'3.99',
+        featured: false,
+        description:'A spice combination of chaat and samosa'
+        },
+        {
+        id: 5,
+        name:'Roles',
+        image: '/assets/images/roles.jpg',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'An unique stye roles'
+        },
+        {
+        id: 6,
+        name:'Bakedish Egg',
+        image: '/assets/images/bakeEgg.jpg',
+        category: 'appetizer',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'An innovative Bakedish with egg on top'
+       },
+       {
+       id: 7,
+       name:'Paneer Tikka',
+       image: '/assets/images/paneertika.jpg',
+       category: 'appetizer',
+       label:'',
+       price:'1.99',
+       featured: false,
+       description:'Paneer tikka serve with tiranga style'
+      }
+
+    ];
